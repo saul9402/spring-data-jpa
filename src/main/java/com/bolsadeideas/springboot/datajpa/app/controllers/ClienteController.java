e67c9e1371a7c0a35735759c351c4b7a14be88f9
Saul Avila <saul_harry_@hotmail.com>
2019-05-14 16:22:30 -0500

    se agrega configuracion para conexion a base de datos mysql.

--- src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/ClienteController.java
@@ -3,7 +3,6 @@ package com.bolsadeideas.springboot.datajpa.app.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
