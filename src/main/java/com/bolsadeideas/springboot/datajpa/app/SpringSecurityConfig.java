e26bbbf39fa1017075ef0796ad956500444c81be
Saul Avila <saul_harry_@hotmail.com>
2019-05-23 18:08:34 -0500

    se agregan controladores y vistas para el manejo de inicio y cierre de sesión.

---
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/SpringSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.bolsadeideas.springboot.datajpa.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+				.antMatchers("/ver/**").hasAnyRole("USER").antMatchers("/uploads/** ").hasAnyRole("USER")
+				.antMatchers("/form/**").hasAnyRole("ADMIN").antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+				.antMatchers("/factura/**").hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").permitAll().and().logout().permitAll();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		PasswordEncoder encoder = passwordEncoder();
+		/*
+		 * Forma 1 UserBuilder users = User.builder().passwordEncoder(password -> {
+		 * return encoder.encode(password); });
+		 */
+
+		/*
+		 * Forma 2 UserBuilder users = User.builder().passwordEncoder(password ->
+		 * encoder.encode(password));
+		 */
+		/*
+		 * Forma 3 la más bonita, :3
+		 */
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+
+		builder.inMemoryAuthentication().withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+				.withUser(users.username("andres").password("12345").roles("USER"));
+
+	}
+
+}
--- src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/ClienteController.java
@@ -55,7 +55,7 @@ public class ClienteController {
 		return "ver";
 	}
 
-	@RequestMapping(value = { "/listar" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "", "/", "/listar" }, method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
 		Pageable pageRequest = PageRequest.of(page, 4);
 		Page<Cliente> clientes = clienteServiceImpl.findAll(pageRequest);
